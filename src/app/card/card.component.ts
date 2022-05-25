c2644af85bc038f992e0c50449e675f54d3915b7
AlexDsmr <ai.danilov.smr@gmail.com>
2022-05-25 19:23:01 +0400

    refact: write a variant of add template and styles inside ...ts file

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -5,4 +5,28 @@ import {Component} from '@angular/core';
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.scss']
 })
+
+//@Component({
+//    selector: 'app-card',
+//    template: `
+//        <div class="card">
+//        <h2>Card component</h2>
+//        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, aperiam!</p>
+//        </div>
+//    `,
+//    styles: [
+//        `
+//        .card {
+//            padding: .5rem 1rem;
+//            border: 1px dashed #ccc;
+//            margin-bottom: 1rem;
+//        }
+//
+//        h2 {
+//            margin-bottom: .5rem;
+//        }
+//        `
+//    ]
+//})
+
 export class CardComponent {}
