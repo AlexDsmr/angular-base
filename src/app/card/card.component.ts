26d8a9527d63acabff923a02f77b64558d8672d4
AlexDsmr <ai.danilov.smr@gmail.com>
2022-05-26 18:25:47 +0400

    feat: add changed img which replace after 3sec by binded var

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-card',
@@ -8,12 +8,18 @@ import {Component} from '@angular/core';
 })
 
 
-export class CardComponent {
+export class CardComponent implements OnInit {
 
     title = 'My Card Title'
     text = 'My sample text'
 
     imgUrl: string = 'https://cdn.iconscout.com/icon/free/png-128/angular-2752246-2285063.png'
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.imgUrl = 'https://img1.freepng.ru/20180407/gse/kisspng-vue-js-javascript-framework-front-and-back-ends-gi-technical-5ac9109e6cb7f5.5357905715231264304453.jpg'
+        }, 3000)
+    }
 
     getInfo() {
         return 'This is my info'
