362404197619465394690389b321fbbb0174c626
AlexDsmr <ai.danilov.smr@gmail.com>
2022-05-26 18:30:09 +0400

    feat: add binded btn

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -12,12 +12,14 @@ export class CardComponent implements OnInit {
 
     title = 'My Card Title'
     text = 'My sample text'
+    isDisabled = true
 
     imgUrl: string = 'https://cdn.iconscout.com/icon/free/png-128/angular-2752246-2285063.png'
 
     ngOnInit() {
         setTimeout(() => {
             this.imgUrl = 'https://img1.freepng.ru/20180407/gse/kisspng-vue-js-javascript-framework-front-and-back-ends-gi-technical-5ac9109e6cb7f5.5357905715231264304453.jpg'
+            this.isDisabled = false
         }, 3000)
     }
 
