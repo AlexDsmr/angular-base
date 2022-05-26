f0d09aada760dfa7c3c429fe68306b5964055a46
DANILOV Aleksandr (SMARTEC) <sr01205@smr.dosmartec.ru>
2022-05-26 13:58:27 +0400

    feat: add extrapolation example

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,30 +3,19 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
-    styleUrls: ['./card.component.scss']
+    styleUrls: ['./card.component.scss'],
+    interpolation:['{{', '}}'] // interpolation like default
 })
 
-//@Component({
-//    selector: 'app-card',
-//    template: `
-//        <div class="card">
-//        <h2>Card component</h2>
-//        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, aperiam!</p>
-//        </div>
-//    `,
-//    styles: [
-//        `
-//        .card {
-//            padding: .5rem 1rem;
-//            border: 1px dashed #ccc;
-//            margin-bottom: 1rem;
-//        }
-//
-//        h2 {
-//            margin-bottom: .5rem;
-//        }
-//        `
-//    ]
-//})
 
-export class CardComponent {}
+export class CardComponent {
+
+    title = 'My Card Title'
+    text = 'My sample text'
+
+    number = 42
+
+    array = [1, 1, 2, 3, 5, 8, 13]
+
+    obj = {'name': 'Alex', 'info':{'age': 28, 'job': 'frontend'}}
+}
