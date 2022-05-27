8662347c4b51d3eae610e5a633f71aa8b7f30e86
DANILOV Aleksandr (SMARTEC) <sr01205@smr.dosmartec.ru>
2022-05-27 10:20:33 +0400

    refact: create card-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { FormComponent } from './form/form.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 registerLocaleData(localeRu, 'ru')
 
@@ -14,11 +16,17 @@ registerLocaleData(localeRu, 'ru')
   declarations: [
     AppComponent,
     CardComponent,
-    FormComponent
+    FormComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    /* RouterModule.forRoot([
+      {path: '/', component: CardComponent},
+      {path: 'form', component: FormsModule,
+    },
+    ]), */
   ],
   providers: [],
   bootstrap: [AppComponent]
