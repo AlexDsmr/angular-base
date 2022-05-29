31847e1b581f4b5cb26e46c7b7e04fea60995db1
AlexDsmr <ai.danilov.smr@gmail.com>
2022-05-29 22:34:45 +0400

    style: align form elements

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -18,11 +18,21 @@ export class FormComponent {
 profileForm = new FormGroup({
   firstName: new FormControl(''),
   lastName: new FormControl(''),
+  address: new FormGroup({
+    street: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zip: new FormControl('')
+  })
 });
 
 onSubmit() {
   // TODO: Use EventEmitter with form value
   console.warn(this.profileForm.value);
 }
+
+/////////////
+  
+
 
 }
