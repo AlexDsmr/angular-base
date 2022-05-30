8ff6915a86557e605e0f25c1d33657f047e892cf
AlexDsmr <ai.danilov.smr@gmail.com>
2022-05-30 21:38:36 +0400

    feat: add validation of form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 
+
+export function allowedNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const allowed = !nameRe.test(control.value);
+    return allowed ? {allowedName: {value: control.value}} : null;
+  };
+}
 
 @Component({
   selector: 'app-form',
@@ -13,8 +20,29 @@ export class FormComponent implements OnInit{
 constructor(private fb: FormBuilder) { }
 ngOnInit(): void {
   this.profileForm.get('location')?.valueChanges.subscribe((location) => {
+    console.log(location)
     this.changeEquip(location)
   });
+
+  this.profileForm = new FormGroup({
+    name: new FormControl(this.profileForm.value.name, [
+      Validators.required,      
+    ]),
+    location: new FormControl(this.profileForm.value.location, [
+      Validators.required,      
+    ]),
+  
+    mail: new FormControl(this.profileForm.value.contact.mail, [
+      Validators.required,
+      allowedNameValidator(/.+@.+\..+/i)
+    ]),
+    phone: new FormControl(this.profileForm.value.contact.phone, [
+      Validators.required,
+      allowedNameValidator(/^([+]?[0-9\s-\(\)]{3,25})*$/i)
+    ]),
+  })
+
+
 }
 
 changeEquip(location: string) {
@@ -31,7 +59,7 @@ locations = ['Russia', 'Europe', 'Asia', 'America', "Other"]
 superPowers = ['Super Speed', 'Levitation', 'Accelerated Healing', 'Gravity Control','Elasticity (Stretchy)']
 
 profileForm = this.fb.group({
-  name: [''],
+  name: [''], 
   alias: [''],
   location: [''],
   equipment: [''],
@@ -41,6 +69,8 @@ profileForm = this.fb.group({
     mail: [''],
   })
 });
+
+
 
 
 onSubmit() {
